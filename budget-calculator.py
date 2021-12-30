b9872aad64222da78bfd28cc9ebe0a43e9c7650b
Robin Svahn <robin.svahn@hotmail.com>
2021-12-30 21:08:47 +0100

    Added function to change categories

--- budget-calculator.py
+++ budget-calculator.py
@@ -22,27 +22,70 @@ def addNewCategory():
     global money_avaialable
     global categories
 
-    category_name = input("Välj ett namn till din kategori: ")
-    while(category_name in categories.keys()):
-        category_name = input(
-            f"Kategorin {category_name} finns redan, var vänlig välj ett annat namn: ")
+    new_category_name = input("Välj ett namn till din kategori: ")
+    while new_category_name in categories.keys():
+        new_category_name = input(
+            f"Kategorin {new_category_name} finns redan, var vänlig välj ett annat namn: ")
 
     category_funds = float(
-        input(f"Hur mycket pengar får användas för den nya kategorin {category_name}: "))
-    while(not isEnoughMoneyAvailable(category_funds)):
+        input(f"Hur mycket pengar får användas för den nya kategorin {new_category_name}: "))
+    while not isEnoughMoneyAvailable(category_funds):
         category_funds = float(input(
             f"Det finns inte tillräckligt med pengar för den här kategorin," +
             f"tillgänglig summa är {money_avaialable}, vad ska vi sätta för gräns istället för den nya kategorin? "))
 
     money_avaialable -= category_funds
-    categories[category_name] = category_funds
+    categories[new_category_name] = category_funds
 
     print(
-        f"Kategorin {category_name} har laggts till med en tillgänglig summa på {category_funds}")
+        f"Kategorin {new_category_name} har laggts till med en tillgänglig summa på {category_funds}")
 
 
 def updateCategory():
-    print("Changing category...")
+    global categories
+    global money_avaialable
+
+    category_to_be_updated = input("Vilken kategori vill du ändra? ")
+    while(category_to_be_updated not in categories.keys()):
+        category_to_be_updated = input(f"Kategorin {category_to_be_updated} finns inte i budgeten," +
+                                       " om du glömt vilka kateogrier som finns, skriv '?': ")
+        if(category_to_be_updated == "?"):
+            print("Kategorierna är:")
+            for category in categories.keys():
+                print(category)
+            category_to_be_updated = input("Vilken kategori vill du ändra? ")
+
+    cateogry_changed = False
+    while not cateogry_changed:
+        print(
+            f"Du har valt kategorin {category_to_be_updated}, dess gräns är satt till {categories[category_to_be_updated]}")
+        valueToBeChanged = input(
+            "Vad vill du ändra, skriv namn för namnet, eller gräns för gränsen: ")
+
+        if(valueToBeChanged == "namn"):
+            new_category_name = input("Vad ska kategorins namn bytas till? ")
+            while(new_category_name in categories.keys()):
+                new_category_name = input(
+                    f"Kategorin {new_category_name} finns redan, var vänlig välj ett annat namn: ")
+
+            categories[new_category_name] = categories[category_to_be_updated]
+            categories.pop(category_to_be_updated)
+            cateogry_changed = True
+            print(
+                f"Kategorin har updaterats, namn: {new_category_name} gräns: {categories[new_category_name]}")
+
+        elif(valueToBeChanged == "gräns"):
+            new_category_limit = input(
+                "Vad ska kategorins gräns ändras till? ")
+            while(not isEnoughMoneyAvailable(new_category_limit)):
+                new_category_limit = float(input(
+                    f"Det finns inte tillräckligt med pengar för den här kategorin," +
+                    f"tillgänglig summa är {money_avaialable}, vad ska vi sätta för gräns istället för den nya kategorin? "))
+
+            categories[category_to_be_updated] = new_category_limit
+            cateogry_changed = True
+            print(
+                f"Kategorin har updaterats, namn: {categories[category_to_be_updated]} gräns: {new_category_limit}")
 
 
 def navigateToChoice(choice: str):
